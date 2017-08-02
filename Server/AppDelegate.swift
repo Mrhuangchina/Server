b45951e22057ee85d85c64457aa74bdc200ea0d4
Mrhuangchina <jianmissyoulove@qq.com>
2017-08-02 13:46:31 +0800

    Initial Commit

---
+++ Server/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Server
+//
+//  Created by MrHuang on 17/8/2.
+//  Copyright © 2017年 Mrhuang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by MrHuang on 17/8/2.
+//  Copyright © 2017年 Mrhuang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
